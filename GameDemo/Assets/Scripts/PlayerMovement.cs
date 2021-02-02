3d17e4f6019f38073d56376fc82fb475fa3f38f1
Migs <serpentdrake.mg@gmail.com>
2021-02-03 03:35:22 +0800

    initialized project added some movement

---
+++ GameDemo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb2d;
+    public int speed;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb2d.velocity = new Vector2(speed, 0);
+            
+        } 
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb2d.velocity = new Vector2(-speed, 0);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 6f);
+        }
+    }
+}
