08f11ecc1038537c80f8d4f250064790369a5dca
Migs <serpentdrake.mg@gmail.com>
2021-02-04 07:14:08 +0800

    added main functionalities

--- GameDemo/Assets/Scripts/PlayerMovement.cs
+++ GameDemo/Assets/Scripts/PlayerMovement.cs
@@ -7,29 +7,59 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb2d;
     public int speed;
+    private Animator anim;
     
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     
     void Update()
     {
-        if(Input.GetKey(KeyCode.D))
+        bool isGrounded = true;
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
+        bool isJumping = false;
+        bool doublejump = false;
+     
+        if( moveInput < 0)
         {
-            rb2d.velocity = new Vector2(speed, 0);
-            
-        } 
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
 
-        if(Input.GetKey(KeyCode.A))
+        if(isGrounded == true && Input.GetKey(KeyCode.W))
         {
-            rb2d.velocity = new Vector2(-speed, 0);
-            transform.localScale = new Vector2(-1, 1);
-        }
-        if(Input.GetKey(KeyCode.W))
-        {
+            isJumping = true;
             rb2d.velocity = new Vector2(rb2d.velocity.x, 6f);
+            anim.SetBool("isJumping", true);
         }
+        else
+        {
+            anim.SetBool("isJumping", false);
+        }
+        if(isJumping == true && Input.GetKey(KeyCode.W))
+        {
+            doublejump = true;
+            anim.SetBool("isJumping", true);
+        }
+        if( doublejump == true && Input.GetKey(KeyCode.W))
+        {
+
+        }
+
+        if (moveInput == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+      
     }
 }
